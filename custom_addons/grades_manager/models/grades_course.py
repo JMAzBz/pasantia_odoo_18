567f5c827bdf4947455ce82cfb84e72e2f717694
jose_benza <josebenza@gmail.com>
2026-09-17 11:46:19 -0300

    [ADD/MOD] grades_course: add state field, domains, required fields; update odoo.conf

--- custom_addons/grades_manager/models/grades_course.py
+++ custom_addons/grades_manager/models/grades_course.py
@@ -5,16 +5,26 @@ class GradesCourse(models.Model):
     _description = 'Grades Course'
     _rec_name = 'nombre'
 
+    def _default_teacher_id(self):
+        teacher = self.env['res.partner'].search(
+            [('is_teacher', '=', True), ('email', '=', 'mainteacher@gmail.com')], limit=1)
+        return teacher.id
+
     nombre = fields.Char(string='Nombre', required=True)
     student_qty = fields.Integer(string='Student quantity')
-    grades_averge = fields.Float(string= 'Grades averange')
-    description = fields.Text(string= 'Description')
-    is_active  = fields.Boolean(string='Is active')
-    course_start = fields.Date(string='Course Start')
+    grades_averge = fields.Float(string='Grades averange')
+    description = fields.Text(string='Description')
+    is_active = fields.Boolean(string='Is active')
+    course_start = fields.Date(string='Course Start', default=fields.Date.context_today)
     course_end = fields.Date(string='Course End')
     last_evaluation_date = fields.Datetime(string='Last evaluation date')
     course_image = fields.Binary(string='Course image')
-    course_shift = fields.Selection([('day','Day'), ('night', 'Night')], string='Course shift')
-    teacher_id = fields.Many2one('res.partner',string='Teacher')
+    course_shift = fields.Selection([('day', 'Day'), ('night', 'Night')], string='Course shift')
+    teacher_id = fields.Many2one('res.partner', string='Teacher', default=_default_teacher_id)
     evalaution_ids = fields.One2many('grades.evaluation', 'course_id', string='Evaluations')
-    student_ids = fields.Many2many('res.partner','grades_course_students_rel', string="Student")
+    student_ids = fields.Many2many('res.partner', 'grades_course_students_rel', string="Student")
+    state = fields.Selection([
+        ('register', 'Register'),
+        ('in_progress', 'In Progress'),
+        ('finished', 'Finished')
+    ], string='State', default='register')
